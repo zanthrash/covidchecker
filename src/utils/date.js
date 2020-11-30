3cace9d898d1c8aa9e3ed436cc1de0f65744df1f
zanthrash <zanthrash@gmail.com>
2020-11-30 17:15:36 -0600

    adding details to add-to-calendar feature

--- src/utils/date.js
+++ src/utils/date.js
@@ -2,6 +2,7 @@ import { formatToTimeZone } from "date-fns-timezone";
 import { isAfter, startOfDay, endOfDay } from "date-fns";
 import AddToCalendar from "@culturehq/add-to-calendar";
 import { BiCalendarPlus } from "react-icons/bi";
+import { stripIndents } from "common-tags";
 
 export const utcToMidwestDate = (date) => {
   const format = "dddd MMMM D, YYYY";
@@ -23,6 +24,12 @@ export const addDateToCalendar = (dateString, eventLabel) => {
     <AddToCalendar
       event={{
         name: eventLabel,
+        details: stripIndents`This is the first day you could return to normal activities,  
+        if you have recovered and have not had a fever for 24 hours, 
+        and your other symptoms are improving. 
+        
+        If you continue to have a fever at this time,
+        continue to isolate until 24 hours have passed since your fever stays below 100.4 F.`,
         location: "My House",
         startsAt: start,
         endsAt: end,
